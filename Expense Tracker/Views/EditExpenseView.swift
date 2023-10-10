b249e048928acda4b98ce0a0e8785ada723dee8a
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-10 07:54:57 -0400

    working on edit expense view

--- Expense Tracker/Views/EditExpenseView.swift
+++ Expense Tracker/Views/EditExpenseView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct EditExpenseView: View {
+    
+    @Environment(\.managedObjectContext) var managedObjContext
+    var expense: FetchedResults<Expense>.Element
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
