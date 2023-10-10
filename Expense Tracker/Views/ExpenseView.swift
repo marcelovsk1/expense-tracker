6c97f2c435f4948936503362c5a7e92d767609a6
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-09 23:36:28 -0400

    starting the expense view

--- Expense Tracker/Views/ExpenseView.swift
+++ Expense Tracker/Views/ExpenseView.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct ExpenseView: View {
+    
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.date, order:
+            .reverse)]) var expenses: FetchedResults<Expense>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                ScrollView()
+                
+            }
+            .padding()
+            .navigationTitle("Expenses")
+        }
     }
 }
 
