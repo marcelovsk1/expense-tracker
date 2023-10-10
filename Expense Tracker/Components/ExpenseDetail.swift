f10f6cb3bae9d9a0b97b405ea6023f29eff7294e
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-10 07:29:44 -0400

    working on expense detail

---
+++ Expense Tracker/Components/ExpenseDetail.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseDetail.swift
+//  Expense Tracker
+//
+//  Created by Marcelo Amaral Alves on 2023-10-09.
+//
+
+import SwiftUI
+
+struct ExpenseDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseDetail()
+}
